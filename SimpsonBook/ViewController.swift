d5c5c517e583f38196b8d83858558a3788ef50ce
yigitkarakurt <yigitkarakurt35@gmail.com>
2022-10-05 20:52:14 +0300

    Initial Commit

---
+++ SimpsonBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpsonBook
+//
+//  Created by Yiğit Karakurt on 5.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
